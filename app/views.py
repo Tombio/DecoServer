90c3c5528b463fe3ecfe56c69b9364ab5500717d
Tomi Lahtinen <tomi.lahtinen@solita.fi>
2017-03-28 23:47:04 +0300

    - Added deco end-point for EANx mixes /ean-deco/oxygen/depth/time - Set last deco stop at 6 meters instead of default 3

--- app/views.py
+++ app/views.py
@@ -1,17 +1,17 @@
 from app import app
 from flask import Flask, jsonify, Response
 import decotengu
-import json
 
 @app.route('/')
 @app.route('/index')
 def index():
     return Response('Usage: /deco-air/[Depth]/[Time]')
 
-@app.route('/deco-air/<int:depth>/<int:time>')
-def deco_air(depth, time):
+@app.route('/deco-ean/<int:ean>/<int:depth>/<int:time>')
+def deco_ean(ean, depth, time):
     engine = decotengu.create()
-    engine.add_gas(0, 21)
+    engine.add_gas(0, ean)
+    engine.last_stop_6m = True
 
     profile = engine.calculate(depth, time)
 
@@ -29,10 +29,16 @@ def deco_air(depth, time):
     return jsonify(stops)
     #return Response(json.dumps(engine.deco_table), mimetype='application/json')
 
+@app.route('/deco-air/<int:depth>/<int:time>')
+def deco_air(depth, time):
+    return deco_ean(21, depth, time)
+
+
 @app.route('/phases-air/<int:depth>/<int:time>')
 def phases_air(depth, time):
     engine = decotengu.create()
     engine.add_gas(0, 21)
+    engine.last_stop_6m = True
 
     profile = engine.calculate(depth, time)
 
