762e02dca13e6ef7e88bce7efd6ff3216d5cb557
Tomi Lahtinen <tomi.lahtinen@solita.fi>
2017-03-28 21:56:34 +0300

    deco-air end point

--- app/views.py
+++ app/views.py
@@ -9,7 +9,7 @@ def index():
     print("Usage: /deco-air/<Depth>/<Time>")
 
 @app.route('/deco-air/<int:depth>/<int:time>')
-def deco(depth, time)
+def deco(depth, time):
     engine = decotengu.create()
     engine.add_gas(0, 21)
 
