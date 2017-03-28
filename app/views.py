40c592e6994cb6ce708edfb767a8652db9f9bf10
Tomi Lahtinen <tomi.lahtinen@solita.fi>
2017-03-28 22:23:17 +0300

    Fixed routes and added function for deco and phases

--- app/views.py
+++ app/views.py
@@ -6,10 +6,31 @@ import json
 @app.route('/')
 @app.route('/index')
 def index():
-    print("Usage: /deco-air/<Depth>/<Time>")
+    return Response('Usage: /deco-air/[Depth]/[Time]')
 
 @app.route('/deco-air/<int:depth>/<int:time>')
-def deco(depth, time):
+def deco_air(depth, time):
+    engine = decotengu.create()
+    engine.add_gas(0, 21)
+
+    profile = engine.calculate(depth, time)
+
+    stops = []
+    print(engine.deco_table.total)
+    for step in profile:
+        # Just stupidly exhaust iterator
+        # of dive profile
+        pass
+
+    for stop in engine.deco_table:
+        print(stop)
+        stops.append({'depth': stop.depth, 'time': stop.time})
+
+    return jsonify(stops)
+    #return Response(json.dumps(engine.deco_table), mimetype='application/json')
+
+@app.route('/phases-air/<int:depth>/<int:time>')
+def phases_air(depth, time):
     engine = decotengu.create()
     engine.add_gas(0, 21)
 
@@ -19,4 +40,4 @@ def deco(depth, time):
     for step in profile:
         steps.append({'phase': step.phase, 'abs_p': step.abs_p, 'time': step.time, 'gas': step.gas, 'data': step.data})
 
-    return Response(json.dumps(steps), mimetype='application/json')
+    return jsonify(steps)
